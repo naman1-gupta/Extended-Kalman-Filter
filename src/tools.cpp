9b74f77b19993c3c5e828dc4f8f892d3900def67
terriblestoryteller <thehoopiefrood42@gmail.com>
2019-01-18 18:26:28 +0530

    rmse done

--- src/tools.cpp
+++ src/tools.cpp
@@ -4,6 +4,7 @@
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+using std::cout;
 
 Tools::Tools() {}
 
@@ -14,6 +15,30 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+  VectorXd rmse(4);
+
+   rmse << 0,0,0,0;
+
+   if(estimations.size() != ground_truth.size() || estimations.size() == 0){
+      cout << "Something is wrong. Either the estimations are empty or estimations and ground truth are not equal.";
+      return rmse;
+   }
+
+   else {
+      for(int i = 0; i < estimations.size(); i++){
+
+         VectorXd residual = estimations[i] - ground_truth[i];
+
+         residual = residual.array() * residual.array();
+         rmse += residual;
+
+      }
+      rmse = rmse/estimations.size();
+
+      rmse = rmse.array().sqrt();
+
+      return rmse;
+   }
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -21,4 +46,5 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    * TODO:
    * Calculate a Jacobian here.
    */
+
 }
